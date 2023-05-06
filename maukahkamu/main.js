6b8c297727007b7bc76754203e23b9c6306729cf
Rayya R <106069353+feeldreams@users.noreply.github.com>
2023-05-06 21:00:43 +0700

    Update main.js

--- maukahkamu/main.js
+++ maukahkamu/main.js
@@ -98,7 +98,6 @@ function kataakhir(){
 }
 function muncultombol2(){fungtom2=1;TombolWA.style="opacity:1;transform:scale(1)";}
 function menuju(){if(fungtom2==1){window.location = "https://api.whatsapp.com/send?phone=&text=" + pesanwhatsapp;}}
-pesanwhatsapp = "Aku mau kok jadi pacar kamu ><";
 
 tinggi = iniakhir.offsetHeight;
 function tentukantinggi(){tinggi = iniakhir.offsetHeight;}
