24f1f6d8d200457ac0b2f70e925814f7f93b4a59
Rayya R <106069353+feeldreams@users.noreply.github.com>
2023-09-23 19:51:55 +0700

    Update script.js

--- kaloada/script.js
+++ kaloada/script.js
@@ -194,3 +194,29 @@ function showDiv() {
             }
             setTimeout(tfkata, 300);
         }
+
+
+window.addEventListener("load", (event) => {
+            var overlay = document.querySelector(".overlay");
+            overlay.style.display = "none";
+            loadfoto();
+            wallpaper.style = "opacity:.3";
+            Content.style = "opacity:1;margin-top:25vh";
+        });
+
+        fungsiAwal = 0;
+        document.getElementById("loveIn").onclick = function () {
+            if (fungsiAwal == 0) {
+                loveIn.style = "transition:all .7s ease;opacity:0";
+                ket.style = "transition:all .7s ease;opacity:0";
+                fungsiAwal = 1;
+                setTimeout(initengahan, 300);
+            }
+        }
+
+        function initengahan() {
+            loveIn.style = "display:none";
+            ket.style = "display:none";
+            Content.style = "opacity:1;margin-top:7vh";
+            setTimeout(pertama, 200);
+        }
